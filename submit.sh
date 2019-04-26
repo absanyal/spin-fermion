53381d7cbfa9bb6682f738d74bfc06a55fe1ef4e
absanyal <amit.sanyal@niser.ac.in>
2019-04-26 21:33:13 +0530

    add compilation line before submission

--- submit.sh
+++ submit.sh
@@ -1,8 +1,9 @@
-for NX in {0..11}
+./compile.sh
+for NX in {0..3}
 do
-    for NY in {0..11}
+    for NY in {0..3}
     do
         echo $NX $NY
-        time ./sf2.out 8 8 12 12 $NX $NY -10.0 10.0 10000 10.0 0.2
+        time ./sf2.out 4 4 4 4 $NX $NY -10.0 10.0 10000 10.0 0.2
     done
 done
