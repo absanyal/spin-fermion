26a6fd7041b530ab8e7c58e58c1570493ffd5882
absanyal <amit.sanyal@niser.ac.in>
2019-04-26 20:17:56 +0530

    edited job script

--- job.sh
+++ job.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=1
-#PBS -N 12_6
+#PBS -N sf
 #PBS -j oe
 #PBS -e error.log
 #PBS -o out.log
 cd $PBS_O_WORKDIR
-./submit.sh
+for NX in {0..11}
+do
+    for NY in {0..11}
+    do
+        qsub ./sf2.out 8 8 12 12 $NX $NY -10.0 10.0 10000 10.0 0.2
+    done
+done
