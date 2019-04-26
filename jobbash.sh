6ffcfd39ed8949b46eebd743462dd33eab19b1ff
absanyal <amit.sanyal@niser.ac.in>
2019-04-26 20:57:37 +0530

    corrected job file

--- jobbash.sh
+++ jobbash.sh
@@ -1,16 +1,16 @@
-for NX in {0..11}
+for NX in {0..31}
 do
-    for NY in {0..11}
+    for NY in {0..31}
     do
-        sub="amit_$NX_$NY"
-        echo "#!/bin/bash" >> $sub
+      	sub="amit_${NX}_${NY}"
+        echo "#!/bin/bash" > $sub
         echo "#PBS -l nodes=1:ppn=1" >> $sub
         echo "#PBS -N sf" >> $sub
         echo "#PBS -j oe" >> $sub
         echo "#PBS -e error.log" >> $sub
         echo "#PBS -o out.log" >> $sub
-        echo "cd $PBS_O_WORKDIR" >> $sub
-        echo "time ./sf2.out 8 8 12 12 $NX $NY -10.0 10.0 10000 10.0 0.2 > output.txt " >> $sub
+        echo 'cd $PBS_O_WORKDIR' >> $sub
+        echo "time ./sf2.out 8 8 32 32 $NX $NY -10.0 10.0 10000 10.0 0.2 > outp$
         qsub $sub
     done
 done
